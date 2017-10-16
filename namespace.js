8eb558ad44b587035574215c7e07a61a1b0f4fde
ibra0206 <32390689+ibra0206@users.noreply.github.com>
2017-10-16 15:22:23 -0400

    Add files via upload

---
+++ namespace.js
@@ -0,0 +1,24 @@
+var IBRA0206 = {
+    init: function(){
+    var box = document.createElement("div");
+    box.classList.add("box");   
+    box.textContent = "IBRA0206";    
+    var boxes = document.querySelector("#boxes");    
+    boxes.appendChild(box);
+    
+    box.addEventListener("click", changeColour);
+    box.addEventListener("mouseover", toggleHighlight);
+    box.addEventListener("mouseon", toggleHighlight)     
+        
+    function changeColour(ev){
+        ev.currentTarget.style.borderColor = "pink";
+        ev.currentTarget.style.backgroundColor = "green";
+    }    
+        
+    function toggleHighlight(ev){
+        ev.currentTarget.classList.toggle("highlight");
+        }
+        
+    }
+   
+}
